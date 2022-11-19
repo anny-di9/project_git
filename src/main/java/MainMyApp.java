c3f7526d90421b5cf041a063a2749105af0903e2
Anna Selyutina <a394216@mail.ru>
2022-11-19 17:57:45 +0300

    добавлена вторая строка

--- src/main/java/MainMyApp.java
+++ src/main/java/MainMyApp.java
@@ -1,6 +1,6 @@
 public class MainMyApp {
     public static void main(String[] args) {
         System.out.println("Hello world");
-        System.out.println("Hello world!222");
+        System.out.println("Hello world!22");
     }
 }
